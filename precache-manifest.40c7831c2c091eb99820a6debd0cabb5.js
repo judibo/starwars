f34ff0eb2f6451ec0c47d11b09f7e385569f3842
Julia Dibo <judibo@Julias-MacBook-Air.local>
2019-05-14 19:30:57 -0300

    Updates

---
+++ precache-manifest.40c7831c2c091eb99820a6debd0cabb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a8f8ce22c02d91599d13246392f40db",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "24c2d8b667b633166bad",
+    "url": "/starwars/static/css/main.ed5d71f8.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/starwars/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "24c2d8b667b633166bad",
+    "url": "/starwars/static/js/main.305c804a.chunk.js"
+  },
+  {
+    "revision": "9cd712b42b70d05ab420",
+    "url": "/starwars/static/js/runtime~main.f4b6364c.js"
+  }
+]);
